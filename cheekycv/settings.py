e93c802e924394fc9533eb13f18edfb72b8e77bd
hana <hdrasko@gmail.com>
2022-07-28 01:42:57 +0800

    ss

--- cheekycv/settings.py
+++ cheekycv/settings.py
@@ -20,7 +20,6 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
